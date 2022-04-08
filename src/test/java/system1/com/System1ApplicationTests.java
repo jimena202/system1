b7dd92993e44a2bcbc62aa51cb5a26456e8cd782
Jimena Narváez <101615158+jimena202@users.noreply.github.com>
2022-04-07 23:21:31 -0500

    mi primer commit

---
+++ src/test/java/system1/com/System1ApplicationTests.java
@@ -0,0 +1,13 @@
+package system1.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class System1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
